7a620fe29c74697b977ed73bb0971988b790e164
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2024-04-23 20:09:49 -0700

    hotfix/Al hacer clic en los botones del HomePage te manda a la respectiva página

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,7 +1,14 @@
 import { Typography } from "@material-tailwind/react";
 import { Button } from "@material-tailwind/react";
+import { useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  const handleLabInventoryClick = () => navigate("/lab-inventory");
+  const handleIndustrialInventoryClick = () =>
+    navigate("/industrial-inventory");
+
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="flex flex-col items-center justify-center p-8">
@@ -9,10 +16,18 @@ function HomePage() {
           ¡Bienvenido a nuestro sitio web de demostración!
         </Typography>
         <div className="mb-4">
-          <Button variant="filled" className="mr-2">
+          <Button
+            variant="filled"
+            className="mr-2"
+            onClick={handleLabInventoryClick}
+          >
             Inventario de laboratorio
           </Button>
-          <Button variant="outlined" className="mr-2">
+          <Button
+            variant="outlined"
+            className="mr-2"
+            onClick={handleIndustrialInventoryClick}
+          >
             Inventario industrial
           </Button>
         </div>
