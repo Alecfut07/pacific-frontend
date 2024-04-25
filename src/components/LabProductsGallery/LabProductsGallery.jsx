5df788f036adc167757ef7c8f7495a60bad82b23
Alec Ortega Cisneros <sistemas_alec@segurosdelascalifornias.local>
2024-04-25 14:42:54 -0700

    hotfix/Agregar logica de paginacion a la galeria de productos de laboratorio

--- src/components/LabProductsGallery/LabProductsGallery.jsx
+++ src/components/LabProductsGallery/LabProductsGallery.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo, useCallback } from "react";
 import {
   Card,
   CardHeader,
@@ -14,6 +14,8 @@ import { LabProducts } from "../../data/LabInventory";
 function LabProductsGallery() {
   const [searchProduct, setSearchProduct] = useState("");
   const [labProducts, setLabProducts] = useState(LabProducts);
+  const [fullData, setFullData] = useState(LabProducts);
+  const [selectedEntries, setSelectedEntries] = useState(6);
   const [modalOpen, setModalOpen] = useState(false);
   const [labProductImages, setLabProductImages] = useState(null);
 
@@ -26,6 +28,40 @@ function LabProductsGallery() {
     setLabProductImages(null);
     setModalOpen(false);
   };
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(labProducts.length / selectedEntries);
+  const numberOfPages = [];
+
+  for (let page = 1; page < totalPages; page++) {
+    numberOfPages.push(page);
+  }
+
+  const handleFirstPage = () => setCurrentPage(1);
+  const handlePreviousPage = () => setCurrentPage(currentPage - 1);
+  const handlePageClick = (pageNumber) => setCurrentPage(pageNumber);
+  const handleNextPage = () => setCurrentPage(currentPage + 1);
+  const handleLastPage = () => setCurrentPage(totalPages);
+
+  const filterFunction = useCallback(
+    (data) => {
+      return data.filter(
+        (item) =>
+          item.name.toLowerCase().includes(searchProduct.toLowerCase()) ||
+          item.description.toLowerCase().includes(searchProduct.toLowerCase()),
+      );
+    },
+    [searchProduct],
+  );
+
+  const currentProductsData = useMemo(() => {
+    const filteredData = filterFunction(labProducts);
+
+    const firstPageIndex = (currentPage - 1) * selectedEntries;
+    const lastPageIndex = firstPageIndex + selectedEntries;
+
+    return filteredData.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage, selectedEntries, labProducts, filterFunction]);
 
   // Version 6 --> OK
   //   return (
@@ -64,7 +100,7 @@ function LabProductsGallery() {
         </CardHeader>
         <CardBody>
           <div className="grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 md:grid-cols-3">
-            {labProducts.map((labProduct) => (
+            {currentProductsData.map((labProduct) => (
               <LabProduct
                 key={labProduct.id}
                 product={labProduct}
