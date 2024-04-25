4e32c11af06bbda7799127ae6a9f4932bc0af0da
Alec Ortega Cisneros <sistemas_alec@segurosdelascalifornias.local>
2024-04-25 15:42:23 -0700

    bugfix/Agregar una imagen y colocarla arriba del componente LabProductsGallery

--- src/pages/Lab/LabInventoryPage/LabInventoryPage.jsx
+++ src/pages/Lab/LabInventoryPage/LabInventoryPage.jsx
@@ -1,8 +1,12 @@
 import LabProductsGallery from "../../../components/LabProductsGallery/LabProductsGallery";
+import labInventoryBackgroundImage from "../../../images/LabInventory/LabInventoryPage/lab-inventory-background.jpeg";
 
 function LabInventoryPage() {
   return (
     <div>
+      <div className="relative">
+        <img src={labInventoryBackgroundImage} className="w-full" />
+      </div>
       <LabProductsGallery />
     </div>
   );
