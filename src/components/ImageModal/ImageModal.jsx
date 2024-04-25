7c7dcf62e7dda32d3d336f07183100a719287b25
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2024-04-24 20:05:59 -0700

    bugfix/Cambiar el color para cerrar el ImageModal

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -28,7 +28,7 @@ function ImageModal({ isOpen, onClose, image }) {
         className="relative w-full max-w-lg rounded-lg bg-white p-6"
       >
         <button
-          className="absolute right-0 top-0 m-4 text-gray-500 hover:text-gray-700"
+          className="absolute right-0 top-0 m-4 text-red-500 hover:text-red-700"
           onClick={onClose}
         >
           <svg
