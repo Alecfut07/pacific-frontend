9aa37309aec4ea1e686e0f3a6a49a0d3d1344560
Alec Ortega Cisneros <sistemas_alec@segurosdelascalifornias.local>
2024-04-25 12:36:27 -0700

    bugfix/Agregar hover a la imagen de cada producto en LabProductsGallery

--- src/components/LabProductsGallery/LabProductsGallery.jsx
+++ src/components/LabProductsGallery/LabProductsGallery.jsx
@@ -19,21 +19,38 @@ function LabProductsGallery() {
     setModalOpen(false);
   };
 
-  // Version 2 --> OK
+  // Version 3 --> OK
   //   return (
   //     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-  //       {labProducts.map((labProduct) => (
-  //         <div key={labProduct.id} className="relative flex flex-col">
+  //       {labProducts.map((labProduct, index) => (
+  //         <div
+  //           key={labProduct.id}
+  //           className={`relative flex flex-col${index % 3 === 2 ? " border-r border-gray-300" : ""}`}
+  //         >
   //           <img
   //             className="h-40 w-40 rounded-lg object-cover"
   //             src={labProduct.images[0]}
   //             alt="gallery-photo"
   //             onClick={() => openModal(labProduct.images)}
   //           />
-  //           <div className="flex flex-grow flex-col justify-between bg-gray-300 p-2">
+  //           <div className="flex flex-grow flex-col justify-between bg-white p-2">
   //             <p className="text-lg font-bold">{labProduct.name}</p>
   //             <p className="text-sm">{labProduct.description}</p>
   //             <p className="text-base font-bold">${labProduct.price} MXN</p>
+  //           </div>
+  //           <div className="flex items-center justify-between">
+  //             <ButtonGroup>
+  //               <Button>-</Button>
+  //               <Input value={0} />
+  //               <Button>+</Button>
+  //             </ButtonGroup>
+  //           </div>
+
+  //           <div className="flex items-center justify-between">
+  //             <Button color="blue" className="flex items-center">
+  //               Agregarlo al carrito
+  //               <ShoppingCartIcon className="ml-2 h-5 w-5" />
+  //             </Button>
   //           </div>
   //         </div>
   //       ))}
@@ -45,7 +62,6 @@ function LabProductsGallery() {
   //     </div>
   //   );
 
-  // Version 3 --> OK
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
       {labProducts.map((labProduct, index) => (
@@ -54,7 +70,7 @@ function LabProductsGallery() {
           className={`relative flex flex-col${index % 3 === 2 ? " border-r border-gray-300" : ""}`}
         >
           <img
-            className="h-40 w-40 rounded-lg object-cover"
+            className="h-40 w-40 rounded-lg border border-blue-gray-50 bg-blue-gray-50/50 object-contain p-1 transition duration-300 ease-in-out hover:border-blue-500 hover:brightness-90"
             src={labProduct.images[0]}
             alt="gallery-photo"
             onClick={() => openModal(labProduct.images)}
