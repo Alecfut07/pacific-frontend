39c79627a511054e318c9e6a56c66dfdcde26643
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2024-04-26 00:07:51 -0700

    feature/Agregar div con texto y con opacidad en las paginas de LabInventoryPage e IndustrialInventoryPage

--- src/pages/Industrial/IndustrialInventoryPage/IndustrialInventoryPage.jsx
+++ src/pages/Industrial/IndustrialInventoryPage/IndustrialInventoryPage.jsx
@@ -6,6 +6,15 @@ function IndustrialInventoryPage() {
     <div>
       <div className="relative">
         <img src={industrialInventoryBackgroundImage} className="w-full" />
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="text-center text-white">
+            <h1 className="text-3xl font-bold">Inventario industrial</h1>
+            <p className="mt-2">
+              Un inventario completo y organizado de suministros y equipos para
+              cosas industriales.
+            </p>
+          </div>
+        </div>
       </div>
       <IndustrialProductsGallery />
     </div>
