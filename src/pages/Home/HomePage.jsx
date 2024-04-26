e1c089a3b739d7e7866fb04b5107f6db7c4ce13c
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2024-04-25 23:43:09 -0700

    hotfix/Agregar 3 Cards para darle estilo al HomePage

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,14 +1,8 @@
-import { Button, Typography } from "@material-tailwind/react";
+import { Button, Card, CardBody, Typography } from "@material-tailwind/react";
 import { useNavigate } from "react-router-dom";
 import homeBackgroundImage from "../../images/Home/home-background.png";
 
 function HomePage() {
-  const navigate = useNavigate();
-
-  const handleLabInventoryClick = () => navigate("/lab-inventory");
-  const handleIndustrialInventoryClick = () =>
-    navigate("/industrial-inventory");
-
   return (
     <div className="bg-gray-200">
       <div className="mx-auto overflow-hidden bg-white shadow-md">
@@ -25,6 +19,40 @@ function HomePage() {
             <p className="text-white">
               Gestiona tu inventario de manera eficiente
             </p>
+          </div>
+        </div>
+      </div>
+      <div className="container mx-auto mt-8">
+        <div className="grid grid-cols-3 gap-4">
+          <div className="col-span-1">
+            <Card>
+              <img className="h-64 w-full object-cover" src="" alt="imagen 1" />
+              <CardBody>
+                <div className="bg-blue-500 p-4 text-white">
+                  <p>Texto del cuadrante 1</p>
+                </div>
+              </CardBody>
+            </Card>
+          </div>
+          <div className="col-span-1">
+            <Card>
+              <img className="h-64 w-full object-cover" src="" alt="imagen 2" />
+              <CardBody>
+                <div className="bg-blue-500 p-4 text-white">
+                  <p>Texto del cuadrante 2</p>
+                </div>
+              </CardBody>
+            </Card>
+          </div>
+          <div className="col-span-1">
+            <Card>
+              <img className="h-64 w-full object-cover" src="" alt="imagen 3" />
+              <CardBody>
+                <div className="bg-blue-500 p-4 text-white">
+                  <p>Texto del cuadrante 3</p>
+                </div>
+              </CardBody>
+            </Card>
           </div>
         </div>
       </div>
