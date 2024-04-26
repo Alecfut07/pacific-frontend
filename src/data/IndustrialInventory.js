adce1ddc38a81948e7f06c73f86a53a361d1149c
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2024-04-25 18:33:50 -0700

    feature/Agregar datos de productos industriales

---
+++ src/data/IndustrialInventory.js
@@ -0,0 +1,227 @@
+export const IndustrialProducts = [
+  {
+    id: 1,
+    images: [],
+    name: "Destornillador Phillips",
+    description: "Destornillador con punta en forma de cruz.",
+    price: "50",
+  },
+  {
+    id: 2,
+    images: [],
+    name: "Martillo",
+    description: "Martillo de tamaño mediano.",
+    price: "120",
+  },
+  {
+    id: 3,
+    images: [],
+    name: "Sierra eléctrica",
+    description: "Sierra eléctrica para cortes precisos.",
+    price: "800",
+  },
+  {
+    id: 4,
+    images: [],
+    name: "Llave de tubo",
+    description: "Llave para apretar o aflojar tuercas y tornillos.",
+    price: "80",
+  },
+  {
+    id: 5,
+    images: [],
+    name: "Cinta métrica",
+    description: "Cinta para medir longitudes y distancias.",
+    price: "40",
+  },
+  {
+    id: 6,
+    images: [],
+    name: "Gato hidráulico",
+    description: "Gato para elevar vehículos y objetos pesados.",
+    price: "500",
+  },
+  {
+    id: 7,
+    images: [],
+    name: "Pistola de calor",
+    description:
+      "Herramienta para aplicar calor en trabajos de soldadura y desoldadura.",
+    price: "300",
+  },
+  {
+    id: 8,
+    images: [],
+    name: "Nivel de burbuja",
+    description:
+      "Instrumento para verificar la horizontalidad o verticalidad de superficies.",
+    price: "60",
+  },
+  {
+    id: 9,
+    images: [],
+    name: "Sierra de mano",
+    description: "Sierra manual para cortes precisos en materiales diversos.",
+    price: "90",
+  },
+  {
+    id: 10,
+    images: [],
+    name: "Llave de impacto",
+    description:
+      "Llave para apretar o aflojar tornillos de gran tamaño con facilidad.",
+    price: "250",
+  },
+  {
+    id: 11,
+    images: [],
+    name: "Pinzas de electricista",
+    description:
+      "Pinzas aisladas para trabajos con cables y conexiones eléctricas.",
+    price: "70",
+  },
+  {
+    id: 12,
+    images: [],
+    name: "Escuadra metálica",
+    description:
+      "Instrumento para trazar ángulos rectos en trabajos de carpintería y construcción.",
+    price: "35",
+  },
+  {
+    id: 13,
+    images: [],
+    name: "Taladro eléctrico",
+    description: "Taladro para perforar distintos materiales con precisión.",
+    price: "200",
+  },
+  {
+    id: 14,
+    images: [],
+    name: "Alicates universales",
+    description:
+      "Alicates versátiles para diversos trabajos de agarre y corte.",
+    price: "50",
+  },
+  {
+    id: 15,
+    images: [],
+    name: "Llave allen",
+    description: "Llave hexagonal para tornillos con cabezas hexagonales.",
+    price: "30",
+  },
+  {
+    id: 16,
+    images: [],
+    name: "Cincel",
+    description:
+      "Herramienta para realizar cortes o marcas en materiales duros.",
+    price: "25",
+  },
+  {
+    id: 17,
+    images: [],
+    name: "Escobilla de alambre",
+    description:
+      "Cepillo de alambre para limpiar superficies oxidadas o sucias.",
+    price: "15",
+  },
+  {
+    id: 18,
+    images: [],
+    name: "Serrucho",
+    description:
+      "Serrucho manual para cortar madera u otros materiales blandos.",
+    price: "40",
+  },
+  {
+    id: 19,
+    images: [],
+    name: "Pala",
+    description: "Pala de mano para cavar o mover tierra, arena, o escombros.",
+    price: "70",
+  },
+  {
+    id: 20,
+    images: [],
+    name: "Lámpara de trabajo",
+    description:
+      "Lámpara portátil para iluminar áreas de trabajo con poca luz.",
+    price: "120",
+  },
+  {
+    id: 21,
+    images: [],
+    name: "Tijeras de corte",
+    description: "Tijeras para cortar distintos materiales con precisión.",
+    price: "20",
+  },
+  {
+    id: 22,
+    images: [],
+    name: "Llave de cadena",
+    description: "Llave ajustable con cadena para tuberías o piezas redondas.",
+    price: "100",
+  },
+  {
+    id: 23,
+    images: [],
+    name: "Lijadora eléctrica",
+    description:
+      "Herramienta para lijar superficies de manera rápida y uniforme.",
+    price: "150",
+  },
+  {
+    id: 24,
+    images: [],
+    name: "Niple",
+    description: "Pieza roscada para unir tuberías o conexiones.",
+    price: "5",
+  },
+  {
+    id: 25,
+    images: [],
+    name: "Maza",
+    description: "Martillo grande para golpear superficies o herramientas.",
+    price: "80",
+  },
+  {
+    id: 26,
+    images: [],
+    name: "Alicate de presión",
+    description:
+      "Alicate con sistema de sujeción ajustable para trabajos de agarre.",
+    price: "60",
+  },
+  {
+    id: 27,
+    images: [],
+    name: "Destornillador plano",
+    description: "Destornillador con punta plana para tornillos de ranura.",
+    price: "40",
+  },
+  {
+    id: 28,
+    images: [],
+    name: "Soplete de gas",
+    description:
+      "Herramienta para aplicar calor en trabajos de soldadura o desoldadura.",
+    price: "70",
+  },
+  {
+    id: 29,
+    images: [],
+    name: "Punta de diamante",
+    description:
+      "Punta de diamante para cortar vidrio u otros materiales duros.",
+    price: "100",
+  },
+  {
+    id: 30,
+    images: [],
+    name: "Grapadora industrial",
+    description:
+      "Grapadora robusta para uso intensivo en trabajos de embalaje.",
+    price: "50",
+  },
+];
