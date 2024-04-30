776139d10caac846f96d9e2c9c73e97e3ed658d2
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2024-04-29 19:45:40 -0700

    feature/Agregar Collapse para cuando la pantalla sea chica se muestra el IconButton y muestre el submenu

--- src/components/CustomNavbar/CustomNavbar.jsx
+++ src/components/CustomNavbar/CustomNavbar.jsx
@@ -1,8 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Navbar, Switch, Typography } from "@material-tailwind/react";
+import {
+  IconButton,
+  Collapse,
+  Navbar,
+  Switch,
+  Typography,
+} from "@material-tailwind/react";
 
 function CustomNavbar() {
+  const [openNav, setOpenNav] = useState(false);
   const [inventoryType, setInventoryType] = useState("");
 
   const navigate = useNavigate();
@@ -24,6 +31,13 @@ function CustomNavbar() {
       navigateToLabInventory();
     }
   };
+
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false),
+    );
+  }, []);
 
   const navList = (
     <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
@@ -81,13 +95,51 @@ function CustomNavbar() {
           Inventario - DEMO
         </Typography>
         <div className="flex items-center gap-4">
-          <div className="mr-4 lg:block">{navList}</div>
+          <div className="mr-4 hidden lg:block">{navList}</div>
           <div className="flex items-center gap-x-1">
             <Typography className="mr-4">{inventoryType}</Typography>
             <Switch onChange={(e) => handleSwitchChange(e)} />
           </div>
         </div>
+        <IconButton
+          variant="text"
+          className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+          ripple={false}
+          onClick={() => setOpenNav(!openNav)}
+        >
+          {openNav ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              className="h-6 w-6"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          )}
+        </IconButton>
       </div>
+      <Collapse open={openNav}>{navList}</Collapse>
     </Navbar>
   );
 }
