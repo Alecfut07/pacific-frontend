4574ce76ad53c19c7dc62e92cc6a0f9737a81ed4
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2024-05-01 15:54:26 -0700

    hotfix/Hacer la funcionalidad de agregar el producto de laboratorio al carrito y tambien en actualizar las propiedades quantity y subtotal

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,64 @@ import LabInventoryPage from "./pages/Lab/LabInventoryPage/LabInventoryPage";
 import IndustrialInventoryPage from "./pages/Industrial/IndustrialInventoryPage/IndustrialInventoryPage";
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
   const [openDrawer, setOpenDrawer] = useState(false);
 
   const openDrawerTop = () => setOpenDrawer(true);
 
   const closeDrawerTop = () => setOpenDrawer(false);
+
+  const addToCart = (product, quantity) => {
+    // Convierte el precio de cadena a número y elimina las comas.
+    const price = parseFloat(product.price.replace(/,/g, ""));
+
+    const existingItemIndex = cartItems.findIndex(
+      (item) => item.product && item.product.id === product.id,
+    );
+
+    if (existingItemIndex !== -1) {
+      // Si el producto ya está en el carrito, actualiza su cantidad.
+      const updatedCartItems = [...cartItems];
+      updatedCartItems[existingItemIndex].totalQuantity += quantity;
+      updatedCartItems[existingItemIndex].product.quantity += quantity;
+
+      // Calcula el subtotal
+      updatedCartItems[existingItemIndex].product.subtotal =
+        price * updatedCartItems[existingItemIndex].totalQuantity;
+      setCartItems(updatedCartItems);
+    } else {
+      // Si el producto no está en el carrito, agrégalo con cantidad 1.
+      // Calcula el subtotal
+      const subtotal = price * quantity;
+      const updatedProduct = {
+        ...product,
+        quantity: quantity,
+        subtotal: subtotal,
+      };
+      setCartItems([
+        ...cartItems,
+        {
+          product: updatedProduct,
+          totalQuantity: quantity,
+        },
+      ]);
+    }
+  };
+
   return (
     <>
       <CustomNavbar openDrawerTop={openDrawerTop} />
       <CustomDrawer
-        placement="top"
+        placement="left"
         open={openDrawer}
         onClose={closeDrawerTop}
       />
       <Routes>
-        <Route path="/" element={<LabInventoryPage />} />
-        <Route path="/lab-inventory" element={<LabInventoryPage />} />
+        <Route path="/" element={<LabInventoryPage addToCart={addToCart} />} />
+        <Route
+          path="/lab-inventory"
+          element={<LabInventoryPage addToCart={addToCart} />}
+        />
         <Route
           path="/industrial-inventory"
           element={<IndustrialInventoryPage />}
