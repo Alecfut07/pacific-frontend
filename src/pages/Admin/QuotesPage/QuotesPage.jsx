dd5310b3a1417c4389f927d9a350d1cf348276f1
Alec Ortega Cisneros <sistemas_alec@segurosdelascalifornias.local>
2024-05-22 16:02:13 -0700

    bugfix/Crear estilo del apartado de cotizaciones para los admins

--- src/pages/Admin/QuotesPage/QuotesPage.jsx
+++ src/pages/Admin/QuotesPage/QuotesPage.jsx
@@ -1,7 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Input,
+  Typography,
+} from "@material-tailwind/react";
+import {
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/24/outline";
 
 function QuotesPage() {
-  return <div>QuotesPage</div>;
+  const [searchQuote, setSearchQuote] = useState("");
+
+  return (
+    <Card className="h-full w-full rounded-none">
+      <CardHeader floated={false} shadow={false} className="rounded-none">
+        <div className="mb-4 flex flex-col justify-between gap-8 md:flex-row md:items-end">
+          <div>
+            <Typography
+              variant="h5"
+              color="blue-gray"
+              className="mb-5 font-bold"
+            >
+              Admin - Cotizaciones
+            </Typography>
+            <Button color="black" className="mr-5">
+              Cerrar sesión
+            </Button>
+            <div className="mt-4 flex items-center">
+              <label className="block text-sm font-medium text-gray-700">
+                Mostrar entradas:&nbsp;
+              </label>
+              <select className="mt-1 block rounded-md border border-gray-300 bg-white px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500">
+                <option value={10}>10</option>
+                <option value={25}>25</option>
+                <option value={50}>50</option>
+                <option value={100}>100</option>
+              </select>
+            </div>
+          </div>
+          <div className="flex w-full shrink-0 gap-2 md:w-max">
+            <div className="w-full md:w-96">
+              <Input
+                label="Buscar"
+                color="blue"
+                icon={<MagnifyingGlassIcon className="h-5 w-5" />}
+              />
+            </div>
+          </div>
+        </div>
+      </CardHeader>
+      <CardBody className="overflow-x-scroll"></CardBody>
+      <CardFooter className="flex items-center justify-center border-t border-blue-gray-50 p-4">
+        <div className="flex items-center justify-center gap-2">
+          <Button
+            variant="text"
+            size="sm"
+            className="flex items-center rounded-full"
+          >
+            Primera
+          </Button>
+          <Button
+            variant="text"
+            size="sm"
+            className="flex items-center rounded-full"
+          >
+            <ArrowLeftIcon strokeWidth={2} className="mr-2 h-4 w-4" /> Anterior
+          </Button>
+          <div className="flex items-center gap-2"></div>
+          <Button
+            variant="text"
+            size="sm"
+            className="flex items-center rounded-full"
+          >
+            Siguiente
+            <ArrowRightIcon strokeWidth={2} className="ml-2 h-4 w-4" />
+          </Button>
+          <Button
+            variant="text"
+            size="sm"
+            className="flex items-center rounded-full"
+          >
+            Último
+          </Button>
+        </div>
+      </CardFooter>
+    </Card>
+  );
 }
 
 export default QuotesPage;
