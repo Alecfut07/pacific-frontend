8aa6ded365a6fe8da1fd041d88419843c1f3a423
Alec Ortega Cisneros <sistemas_alec@segurosdelascalifornias.local>
2024-05-23 09:14:43 -0700

    bugfix/Establecer paginacion en el CardFooter, hacer sign out, establecer numero de entradas, establecer busqueda de datos, establecer conexion a la api para traer los datos de las cotizaciones

--- src/pages/Admin/QuotesPage/QuotesPage.jsx
+++ src/pages/Admin/QuotesPage/QuotesPage.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import {
   Button,
   Card,
   CardHeader,
   CardBody,
   CardFooter,
+  IconButton,
   Input,
   Typography,
 } from "@material-tailwind/react";
@@ -13,9 +15,96 @@ import {
   ArrowRightIcon,
   MagnifyingGlassIcon,
 } from "@heroicons/react/24/outline";
+import { logout } from "../../../services/UserService";
+import { getQuotes } from "../../../services/CotizacionLabService";
 
 function QuotesPage() {
   const [searchQuote, setSearchQuote] = useState("");
+  const [quotesData, setQuotesData] = useState([]);
+  const [fullData, setFullData] = useState([]);
+
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(true);
+
+  const [selectedEntriesValue, setSelectedEntriesValue] = useState(10);
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPages = Math.ceil(quotesData.length / selectedEntriesValue);
+
+  const numberOfPages = [];
+
+  for (let page = 1; page <= totalPages; page++) {
+    numberOfPages.push(page);
+  }
+
+  const handleFirstPage = () => setCurrentPage(1);
+
+  const handlePreviousPage = () => setCurrentPage(currentPage - 1);
+
+  const handlePageClick = (pageNumber) => setCurrentPage(pageNumber);
+
+  const handleNextPage = () => setCurrentPage(currentPage + 1);
+
+  const handleLastPage = () => setCurrentPage(totalPages);
+
+  const handleSelectionChange = (e) => {
+    setSelectedEntriesValue(e.target.value);
+    setCurrentPage(1);
+  };
+
+  const filterFunction = useCallback(
+    (data) => {
+      return data.filter((quote) =>
+        quote.folio.toLowerCase().includes(searchQuote.toLowerCase()),
+      );
+    },
+    [searchQuote],
+  );
+
+  const currentQuoteData = useMemo(() => {
+    const filteredData = filterFunction(quotesData);
+
+    const firstPageIndex = (currentPage - 1) * selectedEntriesValue;
+    const lastPageIndex = firstPageIndex + selectedEntriesValue;
+
+    return filteredData.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage, selectedEntriesValue, quotesData, filterFunction]);
+
+  const handleSignOutClick = async () => {
+    await logout();
+    navigate("/admin");
+  };
+
+  const updateTableData = async () => {
+    try {
+      setLoading(true);
+      const quotes = await getQuotes();
+      setQuotesData(quotes);
+      setFullData(quotes);
+      setLoading(false);
+    } catch (error) {
+      setQuotesData([]);
+      setFullData([]);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        const quotes = await getQuotes();
+        setQuotesData(quotes);
+        setFullData(quotes);
+        handleFirstPage();
+        setLoading(false);
+      } catch (error) {
+        setQuotesData([]);
+        setFullData([]);
+      }
+    })();
+  }, []);
 
   return (
     <Card className="h-full w-full rounded-none">
@@ -29,14 +118,18 @@ function QuotesPage() {
             >
               Admin - Cotizaciones
             </Typography>
-            <Button color="black" className="mr-5">
+            <Button color="black" className="mr-5" onClick={handleSignOutClick}>
               Cerrar sesión
             </Button>
             <div className="mt-4 flex items-center">
               <label className="block text-sm font-medium text-gray-700">
                 Mostrar entradas:&nbsp;
               </label>
-              <select className="mt-1 block rounded-md border border-gray-300 bg-white px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500">
+              <select
+                className="mt-1 block rounded-md border border-gray-300 bg-white px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+                value={selectedEntriesValue}
+                onChange={handleSelectionChange}
+              >
                 <option value={10}>10</option>
                 <option value={25}>25</option>
                 <option value={50}>50</option>
@@ -50,6 +143,8 @@ function QuotesPage() {
                 label="Buscar"
                 color="blue"
                 icon={<MagnifyingGlassIcon className="h-5 w-5" />}
+                value={searchQuote}
+                onChange={(e) => setSearchQuote(e.target.value)}
               />
             </div>
           </div>
@@ -62,6 +157,8 @@ function QuotesPage() {
             variant="text"
             size="sm"
             className="flex items-center rounded-full"
+            onClick={() => handleFirstPage()}
+            disabled={currentPage === 1}
           >
             Primera
           </Button>
@@ -69,14 +166,31 @@ function QuotesPage() {
             variant="text"
             size="sm"
             className="flex items-center rounded-full"
+            onClick={() => handlePreviousPage()}
+            disabled={currentPage === 1}
           >
             <ArrowLeftIcon strokeWidth={2} className="mr-2 h-4 w-4" /> Anterior
           </Button>
-          <div className="flex items-center gap-2"></div>
+          <div className="flex items-center gap-2">
+            {currentQuoteData.length !== 0 &&
+              numberOfPages.map((page, index) => (
+                <IconButton
+                  key={index}
+                  variant={currentPage === index + 1 ? "filled" : "text"}
+                  size="sm"
+                  className="rounded-full"
+                  onClick={() => handlePageClick(page)}
+                >
+                  {page}
+                </IconButton>
+              ))}
+          </div>
           <Button
             variant="text"
             size="sm"
             className="flex items-center rounded-full"
+            onClick={() => handleNextPage()}
+            disabled={currentPage === totalPages || numberOfPages.length === 0}
           >
             Siguiente
             <ArrowRightIcon strokeWidth={2} className="ml-2 h-4 w-4" />
@@ -85,6 +199,8 @@ function QuotesPage() {
             variant="text"
             size="sm"
             className="flex items-center rounded-full"
+            onClick={() => handleLastPage()}
+            disabled={currentPage === totalPages || numberOfPages.length === 0}
           >
             Último
           </Button>
