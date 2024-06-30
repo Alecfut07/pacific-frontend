a6178882997b11d02745be114ee10df2d4dbc287
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2024-06-30 14:49:43 -0700

    hotfix/Agregar el axiosInstance y eliminar NODE_TLS_REJECT_UNAUTHORIZED = 0

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,9 +1,18 @@
 import axios from "axios";
+import https from "https";
 const { VITE_HOST, VITE_API_TOKEN, VITE_USERS } = import.meta.env;
+
+const axiosInstance = axios.create({
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false,
+  }),
+});
 
 export const getUser = async (user_id) => {
   try {
-    const { data } = await axios.get(`${VITE_HOST}${VITE_USERS}${user_id}/`);
+    const { data } = await axiosInstance.get(
+      `${VITE_HOST}${VITE_USERS}${user_id}/`,
+    );
     return data.is_staff;
   } catch (error) {
     console.log(error);
@@ -16,7 +25,10 @@ export const login = async (username, password) => {
       username,
       password,
     };
-    const { data } = await axios.post(`${VITE_HOST}${VITE_API_TOKEN}`, body);
+    const { data } = await axiosInstance.post(
+      `${VITE_HOST}${VITE_API_TOKEN}`,
+      body,
+    );
     return data;
     // const accessToken = response.data;
     // localStorage.setItem("accessToken", accessToken);
@@ -40,7 +52,10 @@ export const signup = async (
       password,
       confirm_password,
     };
-    const { data } = await axios.post(`${VITE_HOST}${VITE_USERS}`, body);
+    const { data } = await axiosInstance.post(
+      `${VITE_HOST}${VITE_USERS}`,
+      body,
+    );
     return data;
   } catch (error) {
     console.error(error);
--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,6 @@ import dotenv from "dotenv";
 // Cargar variables de entorno según el entorno de desarrollo
 dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
 
-if (process.env.NODE_ENV === "production") {
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
